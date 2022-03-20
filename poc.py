b9f526835caf9f88a0402f6a5d87e136a4125562
onMey <50954088+onMey@users.noreply.github.com>
2022-03-20 19:15:11 +0800

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import base64
+import urllib3
 urllib3.disable_warnings()
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0",
